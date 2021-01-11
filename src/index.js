09bfec8a337289d4fbf785877a25dc173b324a03
Oliver Payne <olliepayne@me.com>
2021-01-10 21:52:55 -0700

    backup

--- src/index.js
+++ src/index.js
@@ -1,6 +1,33 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import Game from './components/Game'
+
+const GuessList = (props) => {
+  return (
+    <ul id="guess-list">
+      {props.prevGuesses.map((guess, index) => (
+        <li>{guess}</li>
+      ))}
+    </ul>
+  )
+}
+
+class Game extends React.Component {
+  constructor() {
+    super()
+    this.state = {
+      randomNumber: 0,
+      prevGuesses: []
+    }
+  }
+
+  render() {
+    return (
+      <div id="game-container">
+        <GuessList prevGuesses={this.state.prevGuesses} />
+      </div>
+    )
+  }
+}
 
 ReactDOM.render(
   <React.StrictMode>
